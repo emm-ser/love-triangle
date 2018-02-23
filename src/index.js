0737a50f9f79a19a010a9d872a732b0f892cac3b
emm-ser <106416@bk.ru>
2018-02-23 23:40:27 +0300

    love-triangle/feature: solve

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+    for (var i = 0; i < preferences.length; i++) {
+
+      var a = preferences[i];
+      if (a == 0) continue;
+      var b = preferences[a - 1];
+      if (b == 0) continue;
+      var c = preferences[b - 1];
+      if (c == 0) continue;
+
+      if (a == b || b == c || a == c) {
+        continue;
+      }
+
+      if (i + 1 == c) {
+        count++;
+        preferences[b - 1] = 0;
+        preferences[a - 1] = 0;
+        preferences[i] = 0;
+      }
+    }
+    return count;
 };
